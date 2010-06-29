8c0ec8593aeb2e456cf1f423e84b4910c00580f8
Thomas Hisch <thomas@opentech.at>
2010-06-29 13:36:55 +0200

    it's possible to use either BOOST or std C++0x for TR1 (use BOOST per default)
    Signed-off-by: Thomas Hisch <thomas@opentech.at>

--- global.hpp
+++ global.hpp
@@ -1,7 +1,13 @@
 #ifndef __SCTL_global__
 #define __SCTL_global__
 
-#include <tr1/memory>
-#define STD_TR1 std::tr1
+#ifdef USE_BOOST
+ #include <boost/shared_ptr.hpp>
+ #include <boost/weak_ptr.hpp>
+ #define STD_TR1 boost
+#else
+ #include <tr1/memory>
+ #define STD_TR1 std::tr1
+#endif
 
 #endif 
