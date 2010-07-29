e36f1e19d4cd1e26e2f620126e22108e677e7e4c
Thomas Hisch <thomas@opentech.at>
2010-07-29 19:52:19 +0200

    added commandlist support for IAP_client_cli
    Signed-off-by: Thomas Hisch <thomas@opentech.at>

--- IAP_client_cli.cpp
+++ IAP_client_cli.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 
 #include "IAP_server.hpp"
@@ -9,10 +10,21 @@ using namespace std;
 
 int main (int argc, char *argv[]) {
    if( argc < 2 ){
-      cout << "Usage: " << *argv << " server-hostname\n";
+      cout << "Usage: " << *argv << " server-hostname  [cmd-file]\n";
       exit(1);
    }
    string serverhost = argv[1];
+   ifstream f;
+   bool read_cmd_list = false;
+
+   if(argc==3) {
+       f.open(argv[2]);
+       if(f.is_open() != true) {
+	   cerr << "couldn't open " <<argv[2] << endl;
+	   return 1;
+       }
+       read_cmd_list = true;
+   }
 
    try {
        cout << "trying to connect to " << serverhost << ":" 
@@ -22,7 +34,12 @@ int main (int argc, char *argv[]) {
        while( true ) {
 	   cout << "#> ";
 	   string s;
-	   getline(cin, s, '\n' );
+	   if(read_cmd_list) {
+	       f >> s;
+	       if(f.eof()) break;
+	       cout << s << endl;
+	   } else
+	       getline(cin, s, '\n' );
 
 	   // cout << "got "  << s[0] << s.length() << endl;
 
@@ -33,7 +50,7 @@ int main (int argc, char *argv[]) {
 
 	   if(!s.length()) // empty string
 	       continue;
-	   
+
 	   try {
 	       client_socket << s;
 	       client_socket >> s;
@@ -49,5 +66,7 @@ int main (int argc, char *argv[]) {
        cout << "Exception: " 
 	    << e.get_SockExcept() << endl;
    }
+
+   f.close();
    return 0;   
 }
