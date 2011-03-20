ceb8e9ef6fc447fb56332b0c769dcf5bb1704667
Thomas Hisch <thomas@opentech.at>
2011-03-20 16:59:47 +0100

    license texts added to each source file
    Signed-off-by: Thomas Hisch <thomas@opentech.at>

--- IAPBoard.cpp
+++ IAPBoard.cpp
@@ -1,3 +1,24 @@
+/*
+ * IAPBoard.cpp
+ *
+ * Copyright (c) 2010-2011 Thomas Hisch <thomas at opentech dot at>.
+ *
+ * This file is part of StepperControl.
+ *
+ * StepperControl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * StepperControl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with StepperControl.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <iostream>
 #include <cstdio>
 
